10f316ce40380362b22c6614d8a50bede81906ff
Hamid <mail.hamid.hoseini@gmail.com>
2020-10-24 22:53:09 -0700

    first commit

--- CoreAnimationSampleApp/ViewController.swift
+++ CoreAnimationSampleApp/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import TheAnimation
 
 class ViewController: UIViewController {
 
+    
+    let layer = CALayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        layer.backgroundColor = UIColor.systemRed.cgColor
+        layer.frame = CGRect(x: 100, y: 100, width: 120, height: 120)
+        view.layer.addSublayer(layer)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+            self.animateMovement()
+            self.animateOpacity()
+            self.rotate()
+        }
+    
+    }
+    
+    func rotate() {
+        //let animation = CABasicAnimation(keyPath: "position")
+        let animation = BasicAnimation(keyPath: .transformRotationZ)
+        animation.fromValue = 0
+        animation.toValue = .pi * 12
+        animation.duration = 1
+        animation.timingFunction = .linear
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        animation.animate(in: layer)
+        //layer.add(animation, forKey: nil)
+        
     }
 
-
+    func animateMovement() {
+        //let animation = CABasicAnimation(keyPath: "position")
+        let animation = BasicAnimation(keyPath: .position)
+        animation.fromValue = CGPoint(x: layer.frame.origin.x + (layer.frame.size.width/2),
+                                      y: layer.frame.origin.y + (layer.frame.size.height/2))
+        animation.toValue = CGPoint(x: 300, y: 300)
+        animation.duration = 1
+        animation.timingFunction = .easeInEaseOut
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        animation.animate(in: layer)
+        //layer.add(animation, forKey: nil)
+        
+    }
+    
+    func animateOpacity() {
+        //let animation = CABasicAnimation(keyPath: "opacity")
+        let animation = BasicAnimation(keyPath: .opacity)
+        animation.fromValue = 1
+        animation.toValue = 0
+        animation.duration = 3
+        //animation.timingFunction =
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = false
+        animation.beginTime = CACurrentMediaTime()
+        animation.animate(in: layer)
+        //layer.add(animation, forKey: nil)
+        
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-CoreAnimationSampleApp/Pods-CoreAnimationSampleApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreAnimationSampleApp : NSObject
+@end
+@implementation PodsDummy_Pods_CoreAnimationSampleApp
+@end
